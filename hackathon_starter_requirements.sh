c5c6da5b45215ebaa9ebdae82f223e51835e5397
George Fung <gycf84@yahoo.com>
2016-08-17 17:11:39 -0700

    added sudo to npm install -g
    global npm package install needs sudo

--- hackathon_starter_requirements.sh
+++ hackathon_starter_requirements.sh
@@ -10,7 +10,7 @@ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
 #nodejs dev tools
-npm install -g nodemon
+sudo npm install -g nodemon
 #install mongodb
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
